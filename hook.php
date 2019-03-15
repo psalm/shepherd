88285666d8cbec5b94601a4c23669d01a4a4b2c3
Matt Brown <github@muglug.com>
2019-03-15 00:01:17 -0400

    Add

--- hook.php
+++ hook.php
@@ -8,11 +8,15 @@ if (!isset($payload['pull_request'])) {
 	return;
 }
 
+echo 'got here' . PHP_EOL;
+
 $git_commit_hash = $payload['pull_request']['head']['sha'] ?? null;
 
 if (!$git_commit_hash) {
 	return;
 }
+
+echo 'and here' . PHP_EOL;
 
 if (!preg_match('/^[a-f0-9]+$/', $git_commit_hash)) {
 	throw new \UnexpectedValueException('Bad git commit hash given');
@@ -23,6 +27,8 @@ $github_storage_path = __DIR__ . '/database/github_data/' . $git_commit_hash . '
 if (file_exists($github_storage_path)) {
 	exit;
 }
+
+echo 'and even here' . PHP_EOL;
 
 file_put_contents($github_storage_path, json_encode($payload));
 
