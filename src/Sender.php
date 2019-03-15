46a4ced0b80c6bc63a538e652cea41f7a1fc4fb8
Matt Brown <github@muglug.com>
2019-03-14 20:18:57 -0400

    Use spaces

--- src/Sender.php
+++ src/Sender.php
@@ -4,28 +4,28 @@ namespace Psalm\Spirit;
 
 class Sender
 {
-	public static function send(array $github_data, array $psalm_data) : void
-	{
-		$config_path = __DIR__ . '/../config.json';
+    public static function send(array $github_data, array $psalm_data) : void
+    {
+        $config_path = __DIR__ . '/../config.json';
 
-		if (!file_exists($config_path)) {
-			throw new \UnexpectedValueException('Missing config');
-		}
+        if (!file_exists($config_path)) {
+            throw new \UnexpectedValueException('Missing config');
+        }
 
-		/**
-		 * @var array{reviewer: array{user: string, password: string}}
-		 */
-		$config = json_decode(file_get_contents($config_path), true);
+        /**
+         * @var array{reviewer: array{user: string, password: string}}
+         */
+        $config = json_decode(file_get_contents($config_path), true);
 
-		$client = new \Github\Client();
-		$client->authenticate($config['reviewer']['user'], $config['reviewer']['password'], \Github\Client::AUTH_HTTP_PASSWORD);
+        $client = new \Github\Client();
+        $client->authenticate($config['reviewer']['user'], $config['reviewer']['password'], \Github\Client::AUTH_HTTP_PASSWORD);
 
-		$repository = $github_data['repository']['name'];
-		$repository_owner = $github_data['repository']['owner']['login'];
-		$pull_request_number = $github_data['pull_request']['number'];
+        $repository = $github_data['repository']['name'];
+        $repository_owner = $github_data['repository']['owner']['login'];
+        $pull_request_number = $github_data['pull_request']['number'];
 
-		$head_sha = $github_data['pull_request']['head']['sha'];
-		$base_sha = $github_data['pull_request']['base']['sha'];
+        $head_sha = $github_data['pull_request']['head']['sha'];
+        $base_sha = $github_data['pull_request']['base']['sha'];
 
         $pr_path = dirname(__DIR__) . '/database/pr_reviews/' . parse_url($github_data['pull_request']['html_url'], PHP_URL_PATH);
 
@@ -58,102 +58,126 @@ class Sender
             }
         }
 
-		$diff_url = 'https://github.com/'
-			. $repository_owner . '/'
-			. $repository . '/compare/'
-			. substr($base_sha, 0, 8) . '...' . substr($head_sha, 0, 8) . '.diff';
+        $diff_url = 'https://github.com/'
+            . $repository_owner . '/'
+            . $repository . '/compare/'
+            . substr($base_sha, 0, 8) . '...' . substr($head_sha, 0, 8) . '.diff';
 
-		// Prepare new cURL resource
-		$ch = curl_init($diff_url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        // Prepare new cURL resource
+        $ch = curl_init($diff_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
 
-		// Set HTTP Header for POST request
-		curl_setopt(
-			$ch,
-			CURLOPT_HTTPHEADER,
-			[
-				'Accept: application/vnd.github.v3.diff'
-			]
-		);
+        // Set HTTP Header for POST request
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            [
+                'Accept: application/vnd.github.v3.diff'
+            ]
+        );
 
-		// Submit the POST request
-		$diff_string = curl_exec($ch);
+        // Submit the POST request
+        $diff_string = curl_exec($ch);
 
-		// Close cURL session handle
-		curl_close($ch);
+        // Close cURL session handle
+        curl_close($ch);
 
-		$diff_parser = new \SebastianBergmann\Diff\Parser();
-		$diffs = $diff_parser->parse($diff_string);
+        $diff_parser = new \SebastianBergmann\Diff\Parser();
+        $diffs = $diff_parser->parse($diff_string);
 
-		/** @var array<int, array{severity: string, line_from: int, line_to: int, type: string, message: string,
-		 * 		file_name: string, file_path: string, snippet: string, from: int, to: int,
-		 * 		snippet_from: int, snippet_to: int, column_from: int, column_to: int, selected_text: string}>
-		 */
-		$issues = $psalm_data['issues'];
+        /** @var array<int, array{severity: string, line_from: int, line_to: int, type: string, message: string,
+         *      file_name: string, file_path: string, snippet: string, from: int, to: int,
+         *      snippet_from: int, snippet_to: int, column_from: int, column_to: int, selected_text: string}>
+         */
+        $issues = $psalm_data['issues'];
 
-		$file_comments = [];
+        $file_comments = [];
 
-		foreach ($issues as $issue) {
-			if ($issue['severity'] === 'error') {
-				$file_name = $issue['file_name'];
+        $missed_errors = [];
 
-				foreach ($diffs as $diff) {
-					if ($diff->getTo() === 'b/' . $file_name) {
-						$diff_file_offset = 0;
+        foreach ($issues as $issue) {
+            if ($issue['severity'] !== 'error') {
+                continue;
+            }
 
-						foreach ($diff->getChunks() as $chunk) {
-							$chunk_end = $chunk->getEnd();
-							$chunk_end_range = $chunk->getEndRange();
+            $file_name = $issue['file_name'];
 
-                            if ($issue['line_from'] >= $chunk_end
-								&& $issue['line_from'] < $chunk_end + $chunk_end_range
-							) {
-								$line_offset = 0;
-								foreach ($chunk->getLines() as $i => $chunk_line) {
-									$diff_file_offset++;
+            foreach ($diffs as $diff) {
+                if ($diff->getTo() === 'b/' . $file_name) {
+                    $diff_file_offset = 0;
 
-									if ($chunk_line->getType() !== \SebastianBergmann\Diff\Line::REMOVED) {
-										$line_offset++;
-									}
+                    foreach ($diff->getChunks() as $chunk) {
+                        $chunk_end = $chunk->getEnd();
+                        $chunk_end_range = $chunk->getEndRange();
 
-									if ($issue['line_from'] === $line_offset + $chunk_end - 1) {
-										$file_comments[] = [
-											'path' => $file_name,
-											'position' => $diff_file_offset,
-											'body' => $issue['message'],
-										];
-										break 3;
-									}
-								}
-							} else {
-								$diff_file_offset += count($chunk->getLines());
-							}
-						}
-					}
-				}
-			}
-		}
+                        if ($issue['line_from'] >= $chunk_end
+                            && $issue['line_from'] < $chunk_end + $chunk_end_range
+                        ) {
+                            $line_offset = 0;
+                            foreach ($chunk->getLines() as $i => $chunk_line) {
+                                $diff_file_offset++;
 
-		$review = $client
-			->api('pull_request')
-			->reviews()
-			->create(
-				$repository_owner,
-				$repository,
-				$pull_request_number,
-				[
-					'commit_id' => $head_sha,
-					'body' => 'Psalm has thoughts',
-					'comments' => $file_comments,
+                                if ($chunk_line->getType() !== \SebastianBergmann\Diff\Line::REMOVED) {
+                                    $line_offset++;
+                                }
+
+                                if ($issue['line_from'] === $line_offset + $chunk_end - 1) {
+                                    $file_comments[] = [
+                                        'path' => $file_name,
+                                        'position' => $diff_file_offset,
+                                        'body' => $issue['message'],
+                                    ];
+                                    break 3;
+                                }
+                            }
+                        } else {
+                            $diff_file_offset += count($chunk->getLines());
+                        }
+                    }
+                }
+            }
+
+            $missed_errors[] = $file_name . ':' . $issue['line_from'] . ':' . $issue['column_from'] . ' - ' . $issue['message'];
+        }
+
+        if ($missed_errors) {
+            $comment_text = ($missed_errors ? "\n\n```" . implode("\n", $missed_errors) . '```' : '');
+
+            if ($file_comments) {
+                $message_body = 'Psalm also found errors in other files' . $comment_text;
+            } else {
+                $message_body = 'Psalm found errors in other files' . $comment_text;
+            }
+        } elseif ($file_comments) {
+            $message_body = 'Psalm found some errors';
+        } elseif ($review) {
+            $message_body = 'Psalm didn’t find any errors!';
+        } else {
+            return;
+        }
+
+        $message_body = 'Psalm found errors in other files' . ($missed_errors ? "\n\n```" . implode("\n", $missed_errors) . '```' : '');
+
+        $review = $client
+            ->api('pull_request')
+            ->reviews()
+            ->create(
+                $repository_owner,
+                $repository,
+                $pull_request_number,
+                [
+                    'commit_id' => $head_sha,
+                    'body' => 'Psalm found errors in other files' . ($missed_errors ? "\n\n```" . implode("\n", $missed_errors) . '```' : ''),
+                    'comments' => $file_comments,
                     'event' => 'REQUEST_CHANGES',
-				]
-			);
+                ]
+            );
 
         $pr_path_dir = dirname($pr_path);
 
         mkdir($pr_path_dir, 0777, true);
 
         file_put_contents($pr_path, json_encode($review));
-	}
+    }
 }
