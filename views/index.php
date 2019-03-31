88c4e323150e68e6d1cc852f91b3a1d68483334e
Matthew Brown <github@muglug.com>
2019-03-31 16:50:04 -0400

    End html tags

--- views/index.php
+++ views/index.php
@@ -10,5 +10,7 @@
 <ul>
 <?php foreach (Psalm\Shepherd\Api::getGithubRepositories() as $github_repository) : ?>
 	<li><?php echo $github_repository ?>: <img src="/github/<?php echo $github_repository ?>/coverage.svg"></li>
-<? endforeach ?>
+<? endforeach; ?>
 </ul>
+</body>
+</html>
