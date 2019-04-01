61a581d279c3ef31d87c3d0f563b2a5c62fd3c63
Matthew Brown <github@muglug.com>
2019-03-31 23:46:55 -0400

    Add line break

--- views/index.php
+++ views/index.php
@@ -16,7 +16,7 @@ $github_url = $config->gh_enterprise_url ?: 'https://github.com';
 
 <ul>
 <?php foreach (Psalm\Shepherd\Api::getGithubRepositories() as $github_repository) : ?>
-	<li><a href="<?php echo $github_url . '/' . $github_repository ?>"><?php echo $github_repository ?></a>: <img src="/github/<?php echo $github_repository ?>/coverage.svg"></li>
+	<li><a href="<?php echo $github_url . '/' . $github_repository ?>"><?php echo $github_repository ?></a>:<br><img src="/github/<?php echo $github_repository ?>/coverage.svg"></li>
 <?php endforeach; ?>
 </ul>
 </body>
