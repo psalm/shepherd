36e027055be5e823af63a89a5e3e35fa0c55c214
Matthew Brown <github@muglug.com>
2019-04-20 23:58:10 -0400

    Improve layout

--- views/history.php
+++ views/history.php
@@ -35,7 +35,9 @@ $github_url = $config->gh_enterprise_url ?: 'https://github.com';
 
 	<div class="container front">
 		<div class="coverage_list">
-			<h2>Type coverage history for <a href="<?php echo $github_url . '/' . $github_repository ?>"><?php echo $repository ?></a></h2>
+			<h2><a href="<?php echo $github_url . '/' . $github_repository ?>"><?php echo $repository ?></a></h2>
+			
+			<h3>Type coverage history</h3>
 
 			<table>
 				<thead>
